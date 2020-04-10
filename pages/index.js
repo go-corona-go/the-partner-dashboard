188906987dc4d38338371975eedb302e00fb4035
Akanksha Choudhary <akanksha.ch29@gmail.com>
2020-04-11 03:07:15 +0530

    Connecting Apollo Client to app in progress

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import ApolloClient from 'apollo-boost';
 import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 import Head from 'next/head'
@@ -26,47 +27,6 @@ function Home(){
     </Head>
 
     <SellerDetailsForm />
-
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-      
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-    `}</style>
 
     <style jsx global>{`
       html,
